9378a9f92ad7b15afa5728a9b1cf958b209d8ecb
? <alberto.garcia.2016@upb.edu.co>
2021-08-21 16:31:11 -0500

    Update MirandaAttributes.py
    Attributes per channel and window

--- Python/Modelling/scripts/MirandaAttributes.py
+++ Python/Modelling/scripts/MirandaAttributes.py
@@ -13,48 +13,60 @@ TODO documentation
 
 __author__ = 'intentodemusico'
 #%% Libraries
+import cv2
+import numpy as np
 import pandas as pd
 import scipy.stats as sts
-import univariate as univariate
-import numpy as np
-from scipy.stats.mstats import gmean
 from pandas import DataFrame
+import univariate as univariate
+from scipy.stats.mstats import gmean
 
 #%%
 def hjorth_params(trace):
     return univariate.hjorth(trace)
 
-def attributes(img):
-    x,y,z = img.shape
+def calculations(img,channel):
     #Preprocessing
-    #If image is monochromatic
-    hist = cv2.calcHist([img],[0],None,[256],[0,256])
-    #Else 
-    #for channel in image, below processing, return 3 atr
-    
+    #If image is mono, 0 is passed as channel, else, every channel will be passed independently
+    hist = cv2.calcHist([img],[channel],None,[256],[0,256])
+
     trace=hist.reshape(256)
     
-    #gTrace=trace[trace>0]
+    #Custom trace for avoiding zero log error
+    gTrace=trace[trace>0]
     
+
     #Getting atributes
-    attributes=np.zeros(8)#.astype(object)
+    attributes=np.zeros(8,dtype=np.longdouble)#.astype(object)
     
     #Kurtosis 
-    attributes[0]=str(sts.kurtosis(trace))
+    attributes[0]=sts.kurtosis(trace)
     #Skewness
-    attributes[1]=str(sts.skew(trace))
+    attributes[1]=sts.skew(trace)
     #Std
-    attributes[2]=str(np.std(trace))
+    attributes[2]=np.std(trace)
     #Range
-    attributes[3]=str(np.ptp(trace))
+    attributes[3]=np.ptp(trace)
     #Median 
-    attributes[4]=str(np.median(trace))
+    attributes[4]=np.median(trace)
     #Geometric_Mean 
-    attributes[5]=str(gmean(trace))
+    attributes[5]=gmean(gTrace)
     #Hjorth
     a,mor, comp= hjorth_params(trace)
     #Mobility 
-    attributes[6]=str(mor)
+    attributes[6]=mor
     #Complexity
-    attributes[7]=str(comp)
+    attributes[7]=comp
     return attributes
+
+def attributes(img,windowSize):
+    if(img.shape(0)/windowSize%1!=0): 
+        return False
+    windowCount=img.shape(0)/windowSize
+    channels=1 if len(img.shape)<2 else 3
+    fullAttributes=np.zeros(0,dtype=np.longdouble)
+    for channel in range(channels):
+        for axe0 in range(windowCount):
+            for axe1 in range(windowCount):
+                np.append(fullAttributes,calculations(img[windowSize*axe0:windowSize*(axe0+1),windowSize*axe1:windowSize*(axe1+1)],channel))
+    return fullAttributes
