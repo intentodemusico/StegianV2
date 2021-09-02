6bdf581431955bafe587d1b8b66e7810f525b678
Random name generator <44100370+intentodemusico@users.noreply.github.com>
2021-09-02 05:26:55 -0500

    Filenames

--- Python/Modelling/scripts/MirandaAttributes.py
+++ Python/Modelling/scripts/MirandaAttributes.py
@@ -36,34 +36,6 @@ def hjorth_params(trace):
 
     return activity, mobility, complexity
 
-def geometric_median(X, eps=1e-5):
-    y = np.mean(X, 0)
-
-    while True:
-        D = cdist(X, [y])
-        nonzeros = (D != 0)[:, 0]
-
-        Dinv = 1 / D[nonzeros]
-        Dinvs = np.sum(Dinv)
-        W = Dinv / Dinvs
-        T = np.sum(W * X[nonzeros], 0)
-
-        num_zeros = len(X) - np.sum(nonzeros)
-        if num_zeros == 0:
-            y1 = T
-        elif num_zeros == len(X):
-            return y
-        else:
-            R = (T - y) * Dinvs
-            r = np.linalg.norm(R)
-            rinv = 0 if r == 0 else num_zeros/r
-            y1 = max(0, 1-rinv)*T + min(1, rinv)*y
-
-        if euclidean(y, y1) < eps:
-            return y1
-
-        y = y1
-
 def calculations(img,channel):
     #Preprocessing
     #If image is mono, 0 is passed as channel, else, every channel will be passed independently
